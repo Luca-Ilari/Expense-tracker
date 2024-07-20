d980d4501dc69a7c3ff9a8909c7adbea03e2feda
Luca Ilari <luca.ilari@gmail.com>
2024-07-21 00:08:09 +0200

    Adding wallet component init

---
+++ app/components/withFetch/addWallet.tsx
@@ -0,0 +1,8 @@
+const AddWallet = () => {
+    return (
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        test
+        </h1>
+    )
+}
+export default AddWallet;
--- app/routes/_nav._tnav.wallets.tsx
+++ app/routes/_nav._tnav.wallets.tsx
@@ -1,12 +1,17 @@
+import AddWallet from "~/components/withFetch/addWallet";
+
 export const loader = () => {
     return null
 
 }
 const Wallets = () => {
     return (
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-            Your wallets
-        </h1>
+        <>
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                Your wallets
+            </h1>
+            <AddWallet/>
+        </>
     )
 }
 export default Wallets;
